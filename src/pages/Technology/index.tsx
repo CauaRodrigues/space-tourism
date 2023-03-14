0d8241378437dc8de26bfbd81cc45cadac62fe4b
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-03-14 19:01:51 -0300

    estilos iniciais da página technology

--- src/pages/Technology/index.tsx
+++ src/pages/Technology/index.tsx
@@ -1,47 +1,49 @@
 import React from "react";
 import { data } from "./data";
-import { ContainerTechnology } from "./page.styled";
+import { Styleds as S } from "./page.styled";
 import { sizes } from "../../styles/sizes";
 
 const Technology: React.FC = () => {
 	const sla = data[0];
 
 	return (
-		<ContainerTechnology>
+		<S.ContainerTechnology>
 			<h5>
 				<span className="stage">03</span>SPACE LAUNCH 101
 			</h5>
 
-			<section className="content">
-				<nav>
-					<span>1</span>
-					<span>2</span>
-					<span>3</span>
-				</nav>
+			<section className="container--content">
+				<div className="content">
+					<S.TabsNumbered>
+						<span>1</span>
+						<span>2</span>
+						<span>3</span>
+					</S.TabsNumbered>
 
-				<div className="info">
-					<span className="nav-text">THE TERMINOLOGY…</span>
+					<div className="info">
+						<span className="nav-text">THE TERMINOLOGY…</span>
 
-					<h3>LAUNCH VEHICLE</h3>
+						<h3>LAUNCH VEHICLE</h3>
 
-					<p>
-						A launch vehicle or carrier rocket is a rocket-propelled vehicle
-						used to carry a payload from Earth's surface to space, usually to
-						Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful
-						in operation. Standing 150 metres tall, it's quite an awe-inspiring
-						sight on the launch pad!
-					</p>
+						<p>
+							A launch vehicle or carrier rocket is a rocket-propelled vehicle
+							used to carry a payload from Earth's surface to space, usually to
+							Earth orbit or beyond. Our WEB-X carrier rocket is the most
+							powerful in operation. Standing 150 metres tall, it's quite an
+							awe-inspiring sight on the launch pad!
+						</p>
+					</div>
+
+					<S.Image>
+						<source
+							srcSet={sla.imagesUrl.landscape}
+							media={`(max-width: ${sizes.tablet}px)`}
+						/>
+						<img src={sla.imagesUrl.portrait} alt={sla.name} />
+					</S.Image>
 				</div>
-
-				<picture>
-					<source
-						srcSet={sla.imagesUrl.landscape}
-						media={`(max-width: ${sizes.tablet}px)`}
-					/>
-					<img src={sla.imagesUrl.portrait} alt={sla.name} />
-				</picture>
 			</section>
-		</ContainerTechnology>
+		</S.ContainerTechnology>
 	);
 };
 
--- src/pages/Technology/page.styled.tsx
+++ src/pages/Technology/page.styled.tsx
@@ -1,4 +1,89 @@
 import styled from "styled-components";
 import { MainContainer } from "../../styles/components";
 
-export const ContainerTechnology = styled(MainContainer)``;
+const ContainerTechnology = styled(MainContainer)`
+	padding-right: 0;
+
+	@media (max-width: 1000px) {
+		padding: 0;
+	}
+
+	.container--content {
+		width: 100%;
+		height: min-content;
+		display: flex;
+		justify-content: flex-end;
+	}
+
+	.content {
+		width: 85%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		gap: 2rem;
+
+		.info {
+			width: 55%;
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			justify-content: flex-start;
+			gap: 1.5rem;
+
+			p {
+				width: 80%;
+			}
+
+			@media (max-width: 1000px) {
+				order: 1;
+				flex-direction: column;
+			}
+		}
+
+		@media (max-width: 1000px) {
+			width: 100%;
+			flex-direction: column;
+		}
+	}
+`;
+
+const TabsNumbered = styled.nav`
+	display: flex;
+	flex-direction: column;
+	gap: 1.5rem;
+
+	@media (max-width: 1000px) {
+		order: 1;
+		flex-direction: row;
+	}
+
+	span {
+		width: 60px;
+		height: 60px;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		background-color: transparent;
+		border: 1px solid ${({ theme }) => theme.colors.primary};
+		border-radius: 50%;
+
+		color: ${({ theme }) => theme.colors.primary};
+		text-align: center;
+		font-size: min(2rem, 6vw);
+		font-weight: 400;
+		font-family: "Bellefair", serif;
+		cursor: pointer;
+	}
+`;
+
+const Image = styled.picture`
+	img {
+		@media (max-width: 1000px) {
+			width: 100%;
+		}
+	}
+`;
+
+export const Styleds = { ContainerTechnology, TabsNumbered, Image };
