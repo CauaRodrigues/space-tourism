5af19cbc5d608b28c2976202533db1580cec2f0e
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-03-15 15:50:11 -0300

    remove footer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { ButtonExplore, ContainerHome } from "./page.styled";
 import { Link } from "react-router-dom";
-import { Footer } from "../../components/Footer";
 import changePageTitle from "../../utils/changePageTitle";
 
 const Home: React.FC = () => {
@@ -10,30 +9,26 @@ const Home: React.FC = () => {
 	}, []);
 
 	return (
-		<>
-			<ContainerHome>
-				<section className="hero">
-					<h5>SO, YOU WANT TO TRAVEL TO</h5>
+		<ContainerHome>
+			<section className="hero">
+				<h5>SO, YOU WANT TO TRAVEL TO</h5>
 
-					<h1>SPACE</h1>
+				<h1>SPACE</h1>
 
-					<p>
-						Let’s face it; if you want to go to space, you might as well
-						genuinely go to outer space and not hover kind of on the edge of it.
-						Well sit back, and relax because we’ll give you a truly out of this
-						world experience!
-					</p>
-				</section>
+				<p>
+					Let’s face it; if you want to go to space, you might as well genuinely
+					go to outer space and not hover kind of on the edge of it. Well sit
+					back, and relax because we’ll give you a truly out of this world
+					experience!
+				</p>
+			</section>
 
-				<section className="container-button">
-					<Link to="/destination">
-						<ButtonExplore>Explore</ButtonExplore>
-					</Link>
-				</section>
-			</ContainerHome>
-
-			<Footer />
-		</>
+			<section className="container-button">
+				<Link to="/destination">
+					<ButtonExplore>Explore</ButtonExplore>
+				</Link>
+			</section>
+		</ContainerHome>
 	);
 };
 
